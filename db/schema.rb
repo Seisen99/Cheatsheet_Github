fb660518d3ebc05a4bb3c90cacdea32baca7f02c
Philippe Francisco <lpfrancisco99@gmail.com>
2022-11-21 18:25:53 +0100

    Added better style to devise forms and added a navbar + Scaffold Tip model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_165838) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_172346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tips", force: :cascade do |t|
+    t.string "content"
+    t.string "author"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
