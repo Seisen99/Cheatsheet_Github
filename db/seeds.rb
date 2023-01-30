bdf3e5d08aa6556bc7253ea5be74e0ba46712055
Philippe Francisco <lpfrancisco99@gmail.com>
2023-01-30 22:07:55 +0100

    added stopaudio command

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+user = User.new(
+  first_name: "toto",
+  last_name: "toto",
+  email: "toto@mail.com",
+  password: "123456"
+)
+user.save!
+
+flat = Flat.new(
+name: "toto",
+address: "33 rue de Babylone",
+city: "Paris",
+zip_code: 75007,
+surface: 100,
+rooms: 4,
+year_of_construction: 1929,
+balcony: true,
+garage: false,
+lift: true,
+parking: true,
+investment_date: "1992/29/29",
+investment_amount: 3000000,
+notary_fee: 30000,
+deposit: 150000
+)
+flat.save!
